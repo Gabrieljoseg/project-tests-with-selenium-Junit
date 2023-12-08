f5b72ba015083f3e1589b1406063d9a36cffa8a9
Gabriel José Gomes <85190212+Gabrieljoseg@users.noreply.github.com>
2023-12-08 21:38:22 +0000

    pom.xml ajustado

--- reddit-test/src/test/java/com/example/AppTest.java
+++ reddit-test/src/test/java/com/example/AppTest.java
@@ -1,20 +1,38 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+    private WebDriver driver;
+
     /**
-     * Rigorous Test :-)
+     * 
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/workspaces/project-tests-with-selenium-Junit/reddit-test/chrome-driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.reddit.com");
     }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testTitle() {
+    String expectedTitle = "Reddit - Dive into anything";
+    String actualTitle = driver.getTitle();
+    Assertions.assertEquals(expectedTitle, actualTitle, "O título da página não é o esperado.");
+}
+
 }
